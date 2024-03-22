50806ce5ad4e780eee780804a190c423f836e41d
livecallclass <154664537+livecallclass@users.noreply.github.com>
2024-03-23 00:16:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6756963411:AAHAxM3x5PlSXMLXp3hZyKp_Ih9D_30MfXY",
-             api_id= 29707621,
-             api_hash= "a1f31eeb31a0b9e36f551da6b8b85752"
+             bot_token= "7019235862:AAEsaRL523FwFVDpadt2-Xif4JxR66J6h_U",
+             api_id= 20672305,
+             api_hash= "4d69a0c0143664dff417fb761a90311f"
 )
 
 @bot.on_message(filters.command(["start"]))
