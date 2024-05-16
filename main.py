3c742222a833f19306357b6d42af3723dd30c719
sumit10869 <142525697+sumit10869@users.noreply.github.com>
2024-05-16 21:19:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,14 +24,14 @@ import os
 import math
 
 bot = Client("bot",
-             bot_token= "6047785902:AAFmHAKkyofYcB6yS67nREtc5CKshZ1zhUE",
-             api_id= 24250238,
-             api_hash= "cb3f118ce5553dc140127647edcf3720"
+             bot_token= "7093978447:AAGbpOZab9v_qPkAfjHtxFpi4kyD9QYaAnw",
+             api_id= 22439323,
+             api_hash= "e0e203c8be2c2c58b04d0c59fc82f507"
 )
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(f"Hi 👋 Sir ! How are You?\n\n☞ I'm a High Speed **Txt File** Downloader Bot.\n\n☞ I can Download **Videos & Pdf** From Your **TXT** File.\n\n**𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 :** 🅵🆁🅸🅶🅾 🅴🅼🅿🅸🆁🅴\n")
+    editable = await m.reply_text(f"Hi 👋 Sir ! How are You?\n\n☞ I'm a High Speed **Txt File** Downloader Bot.\n\n☞ I can Download **Videos & Pdf** From Your **TXT** File.\n\n**𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 :** ๛𝐌𝐑๛𝐒𝐀𝐓𝐘𝐀𝐌๛\n")
   
 
 @bot.on_message(filters.command("stop"))
@@ -46,7 +46,7 @@ async def account_login(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if input.document:
         x = await input.download()
-        await bot.send_document(-1002065733031, x)
+        await bot.send_document(-1002064689550, x)
         await input.delete(True)
         file_name, ext = os.path.splitext(os.path.basename(x))
         
@@ -87,7 +87,7 @@ async def account_login(bot: Client, m: Message):
     else:
         b_name = raw_text0
 
-    await editable.edit("**Enter Resolution Ex :** 480 or 720")
+    await editable.edit("**Enter Resolution Ex :** 360 or 480 or 720")
     input2: Message = await bot.listen(editable.chat.id)
     raw_text2 = input2.text
     await input2.delete(True)
@@ -109,7 +109,7 @@ async def account_login(bot: Client, m: Message):
     except Exception:
             res = "UN"
     
-    await editable.edit("**Enter Your Name**\n**Ex : ** 🅵🆁🅸🅶🅾 🅴🅼🅿🅸🆁🅴™")
+    await editable.edit("**Enter Your Name**\n**Ex : ** ๛𝐌𝐑๛𝐒𝐀𝐓𝐘𝐀𝐌๛™")
     input3: Message = await bot.listen(editable.chat.id)
     raw_text3 = input3.text
     await input3.delete(True)
@@ -118,7 +118,7 @@ async def account_login(bot: Client, m: Message):
     else:
         CR = raw_text3
 
-    await editable.edit("Now Send Your **Thumb url**\nEg : `https://telegra.ph/file/7b060bedc493fa9be744f.jpg`\n\nOr Send **no**")
+    await editable.edit("Now Send Your **Thumb url**\nEg : `https://graph.org/file/5e03ce82519cb95379e68.jpg`\n\nOr Send **no**")
     input6 = message = await bot.listen(editable.chat.id)
     raw_text6 = input6.text
     await input6.delete(True)
@@ -181,13 +181,13 @@ async def account_login(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:
-                cc = f'[ 🎬 ] **Vid ID : **{str(count).zfill(3)}\n**Video Title :** {name1}({res}) 『 🅵🆁🅸🅶🅾 🅴🅼🅿🅸🆁🅴™』.mp4\n**Batch Name :** {b_name}\n\n**Extracted By ➤** {CR}'
-                cc1 = f'[ 📕 ] **Pdf ID : **{str(count).zfill(3)}\n**File Title :** {name1} 『 🅵🆁🅸🅶🅾 🅴🅼🅿🅸🆁🅴™ 』.pdf\n**Batch Name :**{b_name}\n\n**Extracted By ➤** {CR}'
+                cc = f'[ 🎬 ] **Vid ID : **{str(count).zfill(3)}\n**Video Title :** {name1}({res}) 『 ๛𝐌𝐑๛𝐒𝐀𝐓𝐘𝐀𝐌๛ 』.mp4\n**Batch Name :** {b_name}\n\n**Extracted By ➤** {CR}'
+                cc1 = f'[ 📕 ] **Pdf ID : **{str(count).zfill(3)}\n**File Title :** {name1} 『 ๛𝐌𝐑๛𝐒𝐀𝐓𝐘𝐀𝐌๛ 』.pdf\n**Batch Name :**{b_name}\n\n**Extracted By ➤** {CR}'
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -1002065733031)
+                        await copy.copy(chat_id = -1002064689550)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
@@ -201,7 +201,7 @@ async def account_login(bot: Client, m: Message):
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
                         copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = -1002065733031)
+                        await copy.copy(chat_id = -1002064689550)
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
@@ -209,7 +209,7 @@ async def account_login(bot: Client, m: Message):
                         time.sleep(e.x)
                         continue
                 else:
-                    prog = await m.reply_text(f"**Downloading:-**\n\n**Title ➤** `{name}`\n**Quality ➤** `{raw_text2}`\n\n**Bot By ➤ **🅵🆁🅸🅶🅾 🅴🅼🅿🅸🆁🅴™")
+                    prog = await m.reply_text(f"**Downloading:-**\n\n**Title ➤** `{name}`\n**Quality ➤** `{raw_text2}`\n\n**Bot By ➤ **๛𝐌𝐑๛𝐒𝐀𝐓𝐘𝐀𝐌๛")
                     res_file = await helper.download_video(url, cmd, name)
                     filename = res_file
                     await prog.delete(True)
@@ -226,7 +226,7 @@ async def account_login(bot: Client, m: Message):
     await m.reply_text("Done ✅")
 @bot.on_message(filters.command(["vpdf"]))
 async def vision_pdf(bot: Client, m: Message):
-    editable = await m.reply_text("Hi 👋 Sir!\n\nHow are You ?\n\n☞ I'm **Vision Pdf** Downloader Bot.\n\n☞ Send ' /vpdf ' Command to Download **Vision IAS** Pdf.\n\n☞ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 : 🅵🆁🅸🅶🅾 🅴🅼🅿🅸🆁🅴\n")
+    editable = await m.reply_text("Hi 👋 Sir!\n\nHow are You ?\n\n☞ I'm **Vision Pdf** Downloader Bot.\n\n☞ Send ' /vpdf ' Command to Download **Vision IAS** Pdf.\n\n☞ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 : ๛𝐌𝐑๛𝐒𝐀𝐓𝐘𝐀𝐌๛\n")
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await input.delete(True)
@@ -267,7 +267,7 @@ async def vision_pdf(bot: Client, m: Message):
             url = links[i][1]
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/","").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").strip()[:57]
             name = f'{str(count).zfill(3)}) {name1}'
-            cc = f'{str(count).zfill(3)}. {name1}.pdf\n\n**Batch:-** {raw_texty}\n\n**Extracted By ➤** 🅵🆁🅸🅶🅾 🅴🅼🅿🅸🆁🅴™ '
+            cc = f'{str(count).zfill(3)}. {name1}.pdf\n\n**Batch:-** {raw_texty}\n\n**Extracted By ➤** ๛𝐌𝐑๛𝐒𝐀𝐓𝐘𝐀𝐌๛ '
             ka = await helper.vision(url, name, cookies)
             await m.reply_document(ka, caption=cc)
             count += 1
